ab5473965faca6b22bc424850175a9a826a9bd55
patongit <90884766+patgit12@users.noreply.github.com>
2023-09-25 14:24:30 -0400

    updating

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-/*module "callbackend" {
+module "callbackend" {
   source = "git@github.com:patgit12/s3backend.git//s3_module/?ref=v1.2.1"
-  } */
-
-module "consul" {
-  source = "git@github.com:hashicorp/example.git"
-}
+  }
