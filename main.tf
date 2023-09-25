697fce2b2131fe89a40cb431c7f98fb653bb0f63
patongit <90884766+patgit12@users.noreply.github.com>
2023-09-25 14:11:58 -0400

    a commit

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
-module "callbackend" {
+/*module "callbackend" {
   source = "git@github.com:patgit12/s3backend.git//s3_module/?ref=v1.2.1"
-  }
+  } */
+
+module "consul" {
+  source = "git@github.com:hashicorp/example.git"
+}
